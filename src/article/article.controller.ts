2478d59298a04f204094fe8f4cc11b2541c4f29e
Rajnald Balog <rajnald94@gmail.com>
2026-09-07 18:31:43 +0300

    feat: create article method

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ArticleService } from './article.service';
+import { AuthGuard } from '@app/user/guards/auth.guard';
+import { User } from '@app/user/decorators/user.decorator';
+import { CreateArticleDto } from './dto/createArticle.dto';
+import { UserEntity } from '@app/user/user.entity';
+import { ArticleEntity } from './article.entity';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
   @Post()
-  async create() {
-    return this.articleService.createArticle();
+  @UseGuards(AuthGuard)
+  async create(
+    @User() currentUser: UserEntity,
+    @Body('article') createArticleDto: CreateArticleDto,
+  ): Promise<ArticleEntity> {
+    return await this.articleService.createArticle(
+      currentUser,
+      createArticleDto,
+    );
   }
 }
