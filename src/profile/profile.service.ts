d82826d8c9f0ad7d830c286ba59f34177a166a0f
Rajnald Balog <rajnald94@gmail.com>
2026-09-10 15:01:51 +0300

    feat: unfollow profile

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -27,7 +27,11 @@ export class ProfileService {
       throw new HttpException('Profile does not exit', HttpStatus.NOT_FOUND);
     }
 
-    return { ...user, following: false };
+    const follow = await this.followRepository.findOne({
+      where: { followerId: currentUserId, followingId: user.id },
+    });
+
+    return { ...user, following: Boolean(follow) };
   }
 
   async followProfile(
@@ -37,6 +41,7 @@ export class ProfileService {
     const user = await this.userRepository.findOne({
       where: { username: profileUsername },
     });
+
     if (!user) {
       throw new HttpException('Profile does not exit', HttpStatus.NOT_FOUND);
     }
@@ -60,6 +65,33 @@ export class ProfileService {
     }
 
     return { ...user, following: true };
+  }
+
+  async unfollowProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: { username: profileUsername },
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exit', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'Follower and following cant be equal',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    await this.followRepository.delete({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
+    return { ...user, following: false };
   }
 
   buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
